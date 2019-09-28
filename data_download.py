b35960eda44f945e096e02817f2ab6061d59e87a
shmalex101 <42148891+shmalex101@users.noreply.github.com>
2019-09-28 10:03:09 -0700

    uncomment spark packages to run on ec2

--- data_download.py
+++ data_download.py
@@ -8,40 +8,56 @@ Created on Mon Sep 16 10:01:06 2019
 
 import requests
 import urllib3.request
-import time
 import bs4
-import os
-#import lzma
 import pandas as pd
 import os
 
-url = 'http://web.mta.info/developers/MTA-Bus-Time-historical-data.html'
-aws_bucket = 's3://mtabusdata'
-r = requests.get(url)
-soup = bs4.BeautifulSoup(r.content,'html.parser')
-#print(soup.prettify())
-main_content = soup.find('div', attrs = {'class': 'container'})
-content = main_content.find_all('a')
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
 
-dat = []
-for link in content:
-    dat.append(link['href'])
-dfname = [item for item in dat if item.startswith('http://s3')]
 
-#this thing right here seems to work. Will expand it out to larger database
-#os.system('curl ' + item + ' | gunzip > ' + item.split('/')[-1][0:-3])
+#s crape file download URL from MTA website
+def url_scrape(url):
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.content,'html.parser')
+    main_content = soup.find('div', attrs = {'class': 'container'})
+    content = main_content.find_all('a')
+    dat = []
+    for link in content:
+        dat.append(link['href'])
+    dfname = [item for item in dat if item.startswith('http://s3')]
+    return dfname
 
-for item in dfname:
-    os.system('curl ' + item + ' |xz -d >'+ item.split('/')[-1][0:-3])
-    os.system('aws s3 cp '+item.split('/')[-1][0:-3] +' '+aws_bucket+'/' \
-              +item.split('/')[-1][0:-3])
-    os.system('rm '+item.split('/')[-1][0:-3])
- 
+if __name__ == "__main__":
     
+    spark = SparkSession \
+        .builder \
+        .appName("Download MTA bus data") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+           
+    url = 'http://web.mta.info/developers/MTA-Bus-Time-historical-data.html'
+    bucket = 's3a://mtabusdata/'
     
-    #os.system('curl ' + item + ' | gunzip > ' + item.split('/')[-1][0:-3])
-    #os.system('aws s3 mv ' + item.split('/')[-1][0:-3]+' '+aws_bucket+'/'+item.split('/')[-1][0:-3])
-    #os.system(' aws s3 mv '+item.split('/')[-1][0:-3]+' '+aws_bucket+'/'+ \
-              #item.split('/')[-1][0:-3])
-    #os.system('rm '+item.split('/')[-1][0:-3])
-    #os.system('curl ' + item + ' | gzip -d > ' + item.split('/')[-1][0:-3])
+    # return url links for MTA bus data
+    dfname = url_scrape(url)
+    dfname = dfname[13:]
+    
+    # download txt file, unpack from xz to txt and save into aws as parquet
+    for item in dfname:
+        filename = item.split('/')[-1][0:-3]
+        try:
+            os.system('curl ' + item + ' |xz -d >'+ filename)
+            df = spark.read.format("csv").option("inferSchema",True). \
+                option("header",True).option("delimiter", "\t").load(filename)
+            df.write.save(bucket+'/parquet/'+filename[0:-4],format='parquet')
+            os.remove(filename)
+        except:
+            print('Error downloading' + filename )
+            pass
+
+        
+
+    
+   
