62be678b03aa20f7695ea4211bc63a8870b4ba94
shmalex101 <42148891+shmalex101@users.noreply.github.com>
2019-10-02 19:20:38 -0700

    previous file misnamed

--- batchprocess/batch_process.py
+++ batchprocess/batch_process.py
@@ -1,124 +1,80 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 23 19:06:28 2019
+Created on Tue Sep 24 13:13:29 2019
 
 @author: monster
 """
 
-import sqlalchemy
+from __future__ import print_function
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+from pyspark.sql.functions import unix_timestamp, from_unixtime, hour
+from pyspark.sql.types import *
+from pyspark.sql.functions import split
+
+import time
+import postgres
+import sys
 import pandas as pd
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table
-from sqlalchemy.dialects import postgresql
-import numpy as np
-import plotly.graph_objs as go
-#import psycopg2
+import math
 
-def connect():
-    #Returns a connection and a metadata object
-    # We connect with the help of the PostgreSQL URL
-    user = 'alex'
-    password = 'password'
-    host = 'ec2-100-20-48-52.us-west-2.compute.amazonaws.com'
-    db = 'postgres'
-    port = 5432
-    url = 'postgresql://{}:{}@{}:{}/{}'
-    url = url.format(user, password, host, port, db)
-    # The return value of create_engine() is our connection object
-    con = sqlalchemy.create_engine(url, client_encoding='utf8')
-    # We then bind the connection to MetaData()
-    #meta = sqlalchemy.MetaData(bind=con, reflect=True)
+def write_to_postgres(out_df, table_name):
+    table = table_name
+    mode = "append"
+    connector = postgres.PostgresConnector()
+    connector.write(out_df, table, mode)
 
-    return con
-
-con = connect()
-
-df = pd.read_sql_query('SELECT * FROM tripstat LIMIT 50',con)
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-                dcc.Dropdown(id='dropdown', options=[
-                    {'label': i, 'value': i} for i in df.route_id.values.tolist()
-                    ], multi=False, placeholder='Route selection...',
-    value='MTA NYCT_Q83'),
-        
-                html.Div([
-                    dcc.Graph(
-                        id='example-graph',
-                        ),
-                dash_table.DataTable(
-                    id='table',
-                    columns=[{"name": i, "id": i} for i in df.columns],
-                    style_table={'overflowX': 'scroll'},
-                    style_cell={
-                                'height': 'auto',
-                                'minWidth': '0px', 'maxWidth': '10px',
-                                'whiteSpace': 'normal'
-    })
-   
-               ])
-                ],style = {'display': 'inline-block', 'width': '50%'})
-
-#plot callback
-@app.callback(
-    dash.dependencies.Output(component_id='example-graph', component_property='figure'),
-    [dash.dependencies.Input(component_id='dropdown', component_property='value')]
-)
-def update_figure(dropdown_value):
-    con = connect()
-    if dropdown_value is None:
-        dff = pd.read_sql_query('SELECT * \
-                            FROM tripstat\
-                            WHERE route_id = \'MTA NYCT_Q83\'',con).sort_values(by=['hour']) 
-        x = dff.hour.to_list()   
-        y= dff.avg_dur.to_list()
-        dff = dff.to_dict('records')        
-        return {
-                'data': [{'x':x, \
-                          'y':y, \
-                          'type': 'bar', \
-                          'name': 'Avg'}],
-                'layout':go.Layout(xaxis={'title':'start time (hour)','range': [-0.5, 23.5]},
-                               yaxis={'title':'trip duration (min)'},
-                               title='Average Trip Duration') 
-        
-                }
-    dff = pd.read_sql_query('SELECT * \
-                            FROM tripstat\
-                            WHERE route_id = \''+str(dropdown_value)+ \
-                            '\'',con).sort_values(by=['hour']) 
-    x = dff.hour.to_list()   
-    y= dff.avg_dur.to_list()
-    dff = dff.to_dict('records')
-    return {
-            'data': [{'x':x, \
-                      'y':y, \
-                      'type': 'bar', \
-                      'name': 'Avg'}],
-            'layout':go.Layout(xaxis={'title':'start time (hour)','range': [-0.5, 23.5]},
-                               yaxis={'title':'trip duration (min)'},
-                               title='Average Trip Duration')
-            }
-
-#Table Callback
-@app.callback(
-    dash.dependencies.Output(component_id='table', component_property='data'),
-    [dash.dependencies.Input(component_id='dropdown', component_property='value')]
-)
-def update_output_div(dropdown_value):
-    con = connect()
-    if dropdown_value is None:
-        return df.to_dict('records')
+if __name__ == "__main__":
+    time_start = time.time()
+    #bucket = 's3a://mtabusdata/'
+    #filename = 'MTA-Bus-Time_.2014-08-01.txt'
+    bucket = 's3a://mtabusdata/parquet/'
+    #filename = 'MTA-Bus-Time_.2014-08-07.txt'
+    filename = '*.txt'
+    #spark = SparkSession \
+    #    .builder \
+    #    .appName("Python Spark SQL basic example") \
+    #    .config("spark.some.config.option", "some-value") \
+    #    .getOrCreate()
+    spark = SparkSession \
+        .builder \
+        .appName("busoptimize") \
+        .getOrCreate()    
+    data = spark.read.load(bucket+filename[0:-4])
+    data.show(5)
+    #data = spark.read.format("csv").option("inferSchema",True). \
+    #        option("header",True).option("delimiter", "\t").load(bucket+filename)
     
-    dff = pd.read_sql_query('SELECT * \
-                            FROM tripstat\
-                            WHERE route_id = \''+str(dropdown_value)+ \
-                            '\' LIMIT 50',con).round(1).sort_values(by=['hour']) 
-    
-    return dff.to_dict('records')
+    #data.printSchema()
+    #data.show()
+    trip = data.groupBy('inferred_trip_id'). \
+        agg(F.max("inferred_route_id").alias("route_id"), \
+            F.min("time_received").alias("min_time"), \
+            F.max("time_received").alias("max_time"))
+    trip.show(5)
+    timeFmt = "yyyy-MM-dd'T'HH:mm:ss"
+    timeDiff = (F.unix_timestamp('max_time', format=timeFmt)-\
+                F.unix_timestamp('min_time', format=timeFmt))/60.
+    #add hour column
+    trip = trip.withColumn('hour',hour(trip['max_time']))
+    #compute trip duration
+    trip = trip.withColumn("duration", timeDiff)
+    #filter out trips more than 20 min and less than 1400 min (~ 1 day)
+    trip = trip.filter(trip.duration > 20).filter(trip.duration<1400)
+    #stats for trip duration
+    tripstat = trip.groupBy('route_id','hour'). \
+        agg(F.min('duration').alias("min_dur"), \
+            F.max('duration').alias("max_dur"), \
+            F.avg('duration').alias("avg_dur"))
+    print(tripstat.show(20))
+    #write_to_postgres(trip,'postgres')
+    #tripstat.show(5)
+    write_to_postgres(tripstat,'tripstat2')
+    spark.stop()
+    time_end = (time.time()-time_start)/60
+    print(time_end)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
