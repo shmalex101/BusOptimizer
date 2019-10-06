d10bec132245eee1c03fc92f082b210a20b9bcf4
shmalex101 <42148891+shmalex101@users.noreply.github.com>
2019-10-06 14:01:18 -0700

    cleanup code with pylint

--- datadownload/data_download.py
+++ datadownload/data_download.py
@@ -5,22 +5,22 @@ Created on Mon Sep 16 10:01:06 2019
 
 @author: monster
 """
-
+import os
 import requests
 import urllib3.request
 import bs4
 import pandas as pd
-import os
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from pyspark.sql.types import *
 
-#scrape file download URL from MTA website
+
 def url_scrape(url):
+    """pull url name for data from mta website"""
     r = requests.get(url)
-    soup = bs4.BeautifulSoup(r.content,'html.parser')
-    main_content = soup.find('div', attrs = {'class': 'container'})
+    soup = bs4.BeautifulSoup(r.content, 'html.parser')
+    main_content = soup.find('div', attrs={'class': 'container'})
     content = main_content.find_all('a')
     dat = []
     for link in content:
@@ -28,26 +28,27 @@ def url_scrape(url):
     dfname = [item for item in dat if item.startswith('http://s3')]
     return dfname
 
-#schema for csv download
+
 def dat_schema():
+    """schema for csv bus data"""
     schema = StructType([
         StructField("latitude", DoubleType(), True),
         StructField("longitude", DoubleType(), True),
         StructField("time_received", TimestampType(), True),
         StructField("vehicle_id", IntegerType(), True),
-        StructField("distance_along_trip",DoubleType() , True),
+        StructField("distance_along_trip", DoubleType(), True),
         StructField("inferred_direction_id", StringType(), True),
-        StructField("inferred_phase",StringType() , True),
-        StructField("inferred_route_id",StringType() , True),
-        StructField("inferred_trip_id",StringType() , True),
-        StructField("next_scheduled_stop_distance",StringType(), True),
+        StructField("inferred_phase", StringType(), True),
+        StructField("inferred_route_id", StringType(), True),
+        StructField("inferred_trip_id", StringType(), True),
+        StructField("next_scheduled_stop_distance", StringType(), True),
         StructField("next_scheduled_stop_id", StringType(), True)])
     return schema
 
-def dat_down(url,bucket):
-    # return url links for MTA bus data
+def dat_down(url, bucket):
+    """return url links for MTA bus data"""
     dfname = url_scrape(url)
-    
+
     # download txt file, unpack from xz to txt and save into aws as parquet
     schema = dat_schema()
     for item in dfname:
@@ -55,8 +56,8 @@ def dat_down(url,bucket):
         try:
             os.system('curl ' + item + ' |xz -d >'+ filename)
             df = spark.read.format("csv"). \
-                option('inferSchema',True). \
-                option('header',True). \
+                option('inferSchema', True). \
+                option('header', True). \
                 option('delimiter', '\t'). \
                 schema(schema). \
                 load(filename)
@@ -64,20 +65,20 @@ def dat_down(url,bucket):
                           format='parquet')
             os.remove(filename)
         except:
-            print('Error downloading' + filename )
+            print('Error downloading ' + filename)
             pass
 
 if __name__ == "__main__":
-    
+
     spark = SparkSession \
         .builder \
         .appName('Download MTA bus data') \
         .config('spark.some.config.option', 'some-value') \
         .getOrCreate()
-           
-    url = 'http://web.mta.info/developers/MTA-Bus-Time-historical-data.html'
-    bucket = 's3a://mtabusdata/'
-    
-    dat_down(url,bucket)
-    
+
+    URL = 'http://web.mta.info/developers/MTA-Bus-Time-historical-data.html'
+    BUCKET = 's3a://mtabusdata/'
+
+    dat_down(URL, BUCKET)
+
     spark.stop()
