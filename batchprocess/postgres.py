c23de275fb048174063ae8afa5267bc0eda9a604
shmalex101 <42148891+shmalex101@users.noreply.github.com>
2019-10-06 13:32:08 -0700

    clean up code with pylint

--- batchprocess/postgres.py
+++ batchprocess/postgres.py
@@ -3,21 +3,24 @@
 """
 Created on Fri Sep 20 15:51:40 2019
 
-@author: monster
+@author: Alex Soloway
+Description: postgres connector for insight data engineering project
 """
 
-from pyspark.sql import DataFrameWriter
 import os
+from pyspark.sql import DataFrameWriter
 
 class PostgresConnector(object):
+    """class to connect to postgres database """
     def __init__(self):
         self.database_name = 'postgres'
         self.hostname = 'ec2-100-20-48-52.us-west-2.compute.amazonaws.com'
-        self.url_connect = "jdbc:postgresql://{hostname}:5432/{db}".format(hostname=self.hostname, db=self.database_name)
+        self.url_connect = "jdbc:postgresql://{hostname}:5432/{db}" \
+            .format(hostname=self.hostname, db=self.database_name)
         self.properties = {"user":'alex',
-                      "password":'password',
-                      "driver": "org.postgresql.Driver"
-                     }
+                           "password":'password',
+                           "driver": "org.postgresql.Driver"
+                          }
 
     def get_writer(self, df):
         return DataFrameWriter(df)
@@ -25,4 +28,3 @@ class PostgresConnector(object):
     def write(self, df, table, mode):
         my_writer = self.get_writer(df)
         my_writer.jdbc(self.url_connect, table, mode, self.properties)
-        
